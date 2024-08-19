f03ea32bf56c4d93825799804658b21ed8c42c6a
Scott W Harden <swharden@gmail.com>
2024-08-19 11:15:45 -0400

    randomize HN article order

--- src/dataSources.js
+++ src/dataSources.js
@@ -3,6 +3,7 @@ function GetHackerNewsText(setMessage, setSpinner, count = 30) {
     fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
         .then(response => response.json())
         .then(topStoryIds => {
+            shuffle(topStoryIds);
             const storyPromises = topStoryIds.slice(0, count).map(id => {
                 return fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(response => response.json());
             });
@@ -13,4 +14,13 @@ function GetHackerNewsText(setMessage, setSpinner, count = 30) {
             setMessage(message);
             setSpinner(false);
         });
+}
+
+function shuffle(array) {
+    let currentIndex = array.length;
+    while (currentIndex != 0) {
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
 }
